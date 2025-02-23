3242d3c46be6b6d4c5cfacf7e90d3894e83b2c74
Szymon Oleszek <szymqwerty@gmail.com>
2025-02-24 00:29:15 +0100

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const uuid = require("uuid");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3005;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
