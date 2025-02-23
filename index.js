8495acd27d32d808b66023e58f4142dc31062004
Szymon Oleszek <szymqwerty@gmail.com>
2025-02-24 00:17:07 +0100

    further adjustments

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
 const express = require("express");
+const uuid = require("uuid");
+const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 
 app.use(express.json());
+
+// Enable CORS for all routes
+app.use(cors());
 
 let todos = [];
 
 // Get all todos
 app.get("/api/todos", (req, res) => {
-  res.json(todos);
+  try {
+    return res.json(todos);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 // Add a new todo
 app.post("/api/todos", (req, res) => {
-  const newTodo = req.body;
-  todos.push(newTodo);
-  res.status(201).json(newTodo);
+  try {
+    const newTodo = req.body;
+    todos.push({ ...newTodo, id: uuid.v4() });
+    return res.status(201).json(newTodo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 // Delete a todo by id
 app.delete("/api/todos/:id", (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  todos = todos.filter((todo) => todo.id !== id);
-  res.status(204).send();
+  try {
+    const id = req.params.id;
+    todos = todos.filter((todo) => todo.id !== id);
+    return res.status(204).send();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(port, () => {
